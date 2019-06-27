7f342c388b9c4baa1eaa690113648414c94ca030
SWIFTKENKA <hunterghost526@gmail.com>
2019-06-27 05:21:01 -0400

    CSS

--- src/TwoSticksInator/Main.java
+++ src/TwoSticksInator/Main.java
@@ -7,9 +7,15 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     static String THEME = "/css/dark-theme.css";
+    private static int WIDTH;
+    private static int HEIGHT;
+    private static int minWIDTH = 1080;
+    private static int minHEIGHT = 605;
 
     private void closeProgram() {
         boolean answer = ConfirmBox.display("Close Application", "Are you sure you want to quit? :(");
@@ -19,15 +25,47 @@ public class Main extends Application {
         }
     }
 
+    private static int scaleBetween(int unscaledNum, int minAllowed, int maxAllowed, int min, int max) {
+        return (maxAllowed - minAllowed) * (unscaledNum - min) / (max - min) + minAllowed;
+    }
+
+    private static void setScreen() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        int maxRange = Math.max(minWIDTH, screenSize.width / 2);
+
+        int w = screenSize.width;
+        int h = screenSize.height;
+
+        if (w >= 3440) {
+            w /= 3;
+        } else if (w >= 2560) {
+            w -= minWIDTH;
+            w -= (2560 - minWIDTH) * 2;
+        } else if (w >= 1920) {
+            w -= (1920 - minWIDTH);
+        } else {
+            w = minWIDTH;
+        }
+
+        WIDTH = w;
+        HEIGHT = minHEIGHT;
+
+        System.out.println(maxRange + ": " + WIDTH + "x" + HEIGHT);
+
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
         primaryStage.setTitle("Two Sticks");
+        setScreen();
 
-        //Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        Scene scene = new Scene(root, 1080, 605);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
         primaryStage.setScene(scene);
 
+        primaryStage.setMinWidth(minWIDTH);
+        primaryStage.setMinHeight(minHEIGHT);
 
         primaryStage.getIcons().add(new Image("TwoSticksInator/what.png"));
 
